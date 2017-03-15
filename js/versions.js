78686add8a01e465e028bf1331a471fdc0fe2d0f
Vijayachandran Mani <vijaycs85@gmail.com>
2017-03-15 00:12:50 +0000

    Initial commit

---
+++ js/versions.js
@@ -0,0 +1,132 @@
+var listCount = 3;
+$(function () {
+  var $table = $('#table');
+  var searchText = getUrlParameter('search');
+  var queryString = Math.floor(Math.random() * 10000) + 1;
+
+  $.get('./project-metadata.json?r=' + queryString, function( result ) {
+    $table.bootstrapTable({
+      searchText: searchText,
+      columns: [
+        {
+          valign: 'middle',
+          sortable: true
+        }
+      ],
+      data: result.metadata
+    });
+    $('[data-field="updated"]').html($.timeago(result.updated));
+  });
+});
+
+/**
+ * Details formatter.
+ *
+ * @param index
+ * @param row
+ * @returns {string}
+ */
+function detailFormatter(index, row) {
+  var html = [];
+  $.each(row, function (key, value) {
+    if (typeof value == 'object' ) {
+      return html.push('<p><u><strong>' + key + '</u></strong></p>' + detailFormatter(key, value));
+    }
+    html.push('<p><b>' + key + ':</b> ' + value + '</p>');
+  });
+  return html.join('');
+}
+
+/**
+ * Provides query param by name.
+ *
+ * @param name
+ * @returns {string}
+ */
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+/**
+ * Status formatter.
+ *
+ * @param value
+ * @param row
+ * @param index
+ * @returns {string}
+ * @constructor
+ */
+function StatusFormatter(value, row, index) {
+  var html = 'Active';
+  if (value != 1) {
+    html = 'Inactive';
+  }
+  return html
+}
+
+/**
+ * Formatter for versions.
+ *
+ * @param value
+ * @param row
+ * @param index
+ * @returns {string}
+ * @constructor
+ */
+function VersionFormatterUrl(value, row, index) {
+  var html = [];
+  var cnt = 0;
+  $.each(value, function (versionID, versionData) {
+    cnt++;
+    if (cnt <= listCount) {
+      html.push('<a href="' + versionData.download_link + '" target="_blank"><span class="btn btn-' + getSecurityStatus(versionData) + ' btn-sm"><span class="glyphicon glyphicon-lock"></span> ' + versionID + '</span></a> ');
+    }
+  });
+  return html.join('');
+}
+
+function getSecurityStatus(versionData) {
+  var attr = versionData.security['@attributes'];
+  var status = 'danger';
+  if (attr != undefined && attr['covered'] == 1) {
+    status = 'success';
+  }
+  return status;
+}
+
+/**
+ *  Link formatter.
+ *
+ * @param value
+ * @param row
+ * @param index
+ * @returns {string}
+ * @constructor
+ */
+function LinkFormatterUrl(value, row, index) {
+  var html = [];
+  var cnt = 0;
+  $.each(value,  function (userID, user) {
+    cnt++;
+    if (cnt <= listCount) {
+      html.push('<a href="https://www.drupal.org/user/' + userID + '" target="_blank">' + user.name + '</a>');
+    }
+  });
+  return html.join(', ');
+}
+
+/**
+ *  Link formatter.
+ *
+ * @param value
+ * @param row
+ * @param index
+ * @returns {string}
+ * @constructor
+ */
+function ModuleFormatterUrl(value, row, index) {
+  return '<a href="https://www.drupal.org/project/' + row.project + '" target="_blank">' + value + '</a>';
+}
